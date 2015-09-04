79ca265850d6b35a735aa7e25a224a7fa722597e
yang <douqu.me@gmail.com>
2015-09-04 08:05:54 -0700

    redirt to user while login

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -40,7 +40,7 @@
       <!--header start-->
       <header class="header white-bg">
               <div class="sidebar-toggle-box">
-                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title=""></div>
               </div>
             <!--logo start-->
             <a href="/dashboard" class="logo">Focus<span>Club</span></a>
